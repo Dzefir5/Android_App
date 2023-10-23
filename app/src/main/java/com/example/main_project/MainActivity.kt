5640dd8e62d5d229f2778b19cce6c3033719903d
Dzefir5 <dzefir228@gmail.com>
2023-10-23 14:27:40 +0300

    StartScreen draft

--- app/src/main/java/com/example/main_project/MainActivity.kt
+++ app/src/main/java/com/example/main_project/MainActivity.kt
@@ -10,15 +10,15 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.main_project.screens.StartScreen
 import com.example.main_project.ui.theme.Main_projectTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
-
-
+            StartScreen()
         }
     }
 }
--- app/src/main/java/com/example/main_project/screens/start_screen.kt
+++ app/src/main/java/com/example/main_project/screens/start_screen.kt
@@ -1,5 +1,12 @@
 package com.example.main_project.screens
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.MutableTransitionState
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.scaleIn
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.with
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -14,7 +21,9 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -30,59 +39,109 @@ import androidx.navigation.NavHostController
 @Preview
 @Composable
 fun StartScreen(/*navController: NavHostController*/){
+    val start_state = remember {
+        MutableTransitionState(false).apply {
+            targetState = true
+        }
+    }
+    val state_01 = remember {
+        MutableTransitionState(false).apply {
+            targetState = true
+        }
+    }
     Box(modifier = Modifier
         .fillMaxSize()
-        .background(brush = Brush.verticalGradient(listOf(Color.Blue, Color.White)))
+        .background(brush = Brush.verticalGradient(colorStops = arrayOf(0.0f to Color.Green,0.7f to Color.White)))
     ){}
     Column(modifier = Modifier
         .fillMaxSize(),
         verticalArrangement = Arrangement.Bottom
     ) {
-        Card(
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(400.dp)
-                .align(Alignment.CenterHorizontally),
-            shape = RoundedCornerShape(40.dp,40.dp,0.dp,0.dp),
-            elevation = CardDefaults.cardElevation(10.dp)
-        ){
+        AnimatedVisibility(
+            visibleState = start_state,
+            enter = slideInVertically(
+                animationSpec = tween(durationMillis = 600, delayMillis = 100), initialOffsetY = {it}
+            )
+        ) {
             Card(
                 modifier = Modifier
+                    .fillMaxWidth()
+                    .height(400.dp)
+                    .align(Alignment.CenterHorizontally),
+                shape = RoundedCornerShape(40.dp, 40.dp, 0.dp, 0.dp),
+                colors = CardDefaults.cardColors(Color.White)
+            ) {
+                AnimatedVisibility(modifier = Modifier
                     .height(64.dp)
                     .width(263.dp)
                     .align(Alignment.CenterHorizontally)
                     .offset(y = 200.dp),
-                shape = RoundedCornerShape(21.dp),
-                colors = CardDefaults.cardColors(Color.Blue)
-            ){}
-            Card(
-                modifier = Modifier
+                    visibleState = state_01,
+                    enter = scaleIn(animationSpec = tween(durationMillis = 300, delayMillis = 450))
+                ){
+                    Card(
+                        modifier = Modifier
+                            .height(64.dp)
+                            .width(263.dp)
+                            .align(Alignment.CenterHorizontally),
+                        shape = RoundedCornerShape(21.dp),
+                        colors = CardDefaults.cardColors(Color.Green)
+                    ) {}
+                }
+
+                AnimatedVisibility(modifier = Modifier
                     .height(23.dp)
                     .width(304.dp)
                     .align(Alignment.CenterHorizontally)
                     .offset(y = 300.dp),
-                shape = RoundedCornerShape(11.dp),
-                colors = CardDefaults.cardColors(Color.Blue)
-            ){}
+                    visibleState = state_01,
+                    enter = scaleIn(animationSpec = tween(durationMillis = 300, delayMillis = 450))
+                ){
+                    Card(
+                        modifier = Modifier
+                            .height(23.dp)
+                            .width(304.dp)
+                            .align(Alignment.CenterHorizontally),
+                        shape = RoundedCornerShape(11.dp),
+                        colors = CardDefaults.cardColors(Color.Green)
+                    ) {}
+                }
+
+            }
         }
     }
-    Box(modifier = Modifier
-        .fillMaxSize()
-        //.background(brush = Brush.radialGradient(listOf(Color.Blue, Color.White.copy(0f)))),
-    ){
+
+
+    AnimatedVisibility(
+        visibleState = start_state,
+        enter = slideInVertically(animationSpec = tween(600), initialOffsetY = {it})
+    ) {
         Box(modifier = Modifier
-            .size(330.dp)
-            .background(brush = Brush.radialGradient(listOf(Color.Blue.copy(0.5f), Color.White.copy(0f))))
-            .align(alignment = Alignment.Center)
+            .fillMaxSize()
+            //.background(brush = Brush.radialGradient(listOf(Color.Blue, Color.White.copy(0f)))),
         ){
             Box(modifier = Modifier
-                .size(220.dp)
+                .size(330.dp)
+                .background(
+                    brush = Brush.radialGradient(
+                        listOf(
+                            Color.Green.copy(0.5f),
+                            Color.White.copy(0f)
+                        )
+                    )
+                )
                 .align(alignment = Alignment.Center)
-                .clip(CircleShape)
-                .background(Color.White)
-            )
+            ){
+                Box(modifier = Modifier
+                    .size(220.dp)
+                    .align(alignment = Alignment.Center)
+                    .clip(CircleShape)
+                    .background(Color.White)
+                )
+            }
         }
     }
+
 
 
 }
